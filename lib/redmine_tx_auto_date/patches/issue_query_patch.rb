41f409b359846817f16a45ca437db5e123d98f44
KiHyun Kang <testors@gmail.com>
2026-01-26 20:13:34 +0900

    Update redmine_tx_auto_date - 2026-01-26 20:13:34

--- lib/redmine_tx_auto_date/patches/issue_query_patch.rb
+++ lib/redmine_tx_auto_date/patches/issue_query_patch.rb
@@ -3,26 +3,15 @@ module RedmineTxAutoDate
     module IssueQueryPatch
       def self.included(base)
         base.class_eval do
-          # Add columns to available columns
-          add_available_column TimestampQueryColumn.new(:begin_time, 
-            :sortable => "#{Issue.table_name}.begin_time",
-            :default_order => 'desc', 
-            :groupable => true)
-          
-          add_available_column TimestampQueryColumn.new(:end_time,
-            :sortable => "#{Issue.table_name}.end_time", 
-            :default_order => 'desc',
-            :groupable => true)
-          
-          add_available_column TimestampQueryColumn.new(:confirm_time,
-            :sortable => "#{Issue.table_name}.confirm_time",
-            :default_order => 'desc', 
-            :groupable => true)
+          extend TxBaseHelper::IssueQueryColumnHelper
+          include TxBaseHelper::IssueQueryColumnHelper
 
-          add_available_column QueryColumn.new(:worker,
-            :sortable => lambda {User.fields_for_order_statement("workers")},
-            :groupable => true)
-            
+          # Add columns to available columns
+          add_issue_timestamp_column :begin_time
+          add_issue_timestamp_column :end_time
+          add_issue_timestamp_column :confirm_time
+          add_issue_column :worker, sortable: lambda {User.fields_for_order_statement("workers")}
+
           # Override the initialize_available_filters method
           alias_method :initialize_available_filters_without_tx_auto_date, :initialize_available_filters
           alias_method :initialize_available_filters, :initialize_available_filters_with_tx_auto_date
@@ -31,17 +20,16 @@ module RedmineTxAutoDate
 
       def initialize_available_filters_with_tx_auto_date
         initialize_available_filters_without_tx_auto_date
-        
+
         # Add date filters
-        add_available_filter "begin_time", :type => :date_past
-        add_available_filter "end_time", :type => :date_past  
-        add_available_filter "confirm_time", :type => :date_past
-        
+        add_issue_date_filter :begin_time
+        add_issue_date_filter :end_time
+        add_issue_date_filter :confirm_time
+
         # Add worker filter (same as assigned_to_id for consistency)
-        add_available_filter(
-          "worker_id",
-          :type => :list_optional_with_history, :values => lambda {assigned_to_values}
-        )
+        add_issue_filter "worker_id",
+          type: :list_optional_with_history,
+          values: lambda {assigned_to_values}
       end
     end
   end
