c11d779310c39d48772556ed02e760d607773494
KiHyun Kang <testors@gmail.com>
2026-01-29 17:09:48 +0900

    Update redmine_tx_auto_date - 2026-01-29 17:09:48

--- db/migrate/1_add_begin_end_to_issues.rb
+++ db/migrate/1_add_begin_end_to_issues.rb
@@ -1,16 +1,15 @@
 class AddBeginEndToIssues < ActiveRecord::Migration[5.2]
-    def up
-      add_column :issues, :begin_time, :datetime
-      add_column :issues, :confirm_time, :datetime
-      add_column :issues, :end_time, :datetime
-      add_column :issues, :worker_id, :integer
-    end
-  
-    def down
-      remove_column :issues, :begin_time
-      remove_column :issues, :confirm_time
-      remove_column :issues, :end_time
-      remove_column :issues, :worker_id
-    end
-  end 
-  
+  def up
+    add_column :issues, :begin_time, :datetime unless column_exists?(:issues, :begin_time)
+    add_column :issues, :confirm_time, :datetime unless column_exists?(:issues, :confirm_time)
+    add_column :issues, :end_time, :datetime unless column_exists?(:issues, :end_time)
+    add_column :issues, :worker_id, :integer unless column_exists?(:issues, :worker_id)
+  end
+
+  def down
+    remove_column :issues, :begin_time if column_exists?(:issues, :begin_time)
+    remove_column :issues, :confirm_time if column_exists?(:issues, :confirm_time)
+    remove_column :issues, :end_time if column_exists?(:issues, :end_time)
+    remove_column :issues, :worker_id if column_exists?(:issues, :worker_id)
+  end
+end
